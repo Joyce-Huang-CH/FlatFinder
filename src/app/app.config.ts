2bd4cb710ae0d12ab5f04e0b3598c27975aadd00
joyce273034 <joyce273034@gmail.com>
2024-12-30 20:39:44 -0800

    add firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"first-project-91685","appId":"1:80600432558:web:9345f3a198ce2ea948d23f","storageBucket":"first-project-91685.firebasestorage.app","apiKey":"AIzaSyC8q0ZIZhIRMLM5Cwk7mab83aUgBhkIM88","authDomain":"first-project-91685.firebaseapp.com","messagingSenderId":"80600432558","measurementId":"G-Y294CGF7FY"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideStorage(() => getStorage())]
 };
