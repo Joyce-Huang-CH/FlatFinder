e52d96f639dfffa3ad2bef9ac8cbe340cae12bb4
joyce273034 <joyce273034@gmail.com>
2025-01-23 16:01:34 -0800

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,5 @@
 export const environment = {
   firebase: {
-    apiKey: "AIzaSyC8q0ZIZhIRMLM5Cwk7mab83aUgBhkIM88",
-    authDomain: "first-project-91685.firebaseapp.com",
-    projectId: "first-project-91685",
-    storageBucket: "first-project-91685.firebasestorage.app",
-    messagingSenderId: "80600432558",
-    appId: "1:80600432558:web:9345f3a198ce2ea948d23f",
-    measurementId: "G-Y294CGF7FY"
+    
   },
 };
